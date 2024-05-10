e048a5fc88c4e238cc256661bb6f9a4d7849fd6e
thagoo <lohitgowda56@gmail.com>
2024-05-10 21:52:45 +0530

    database: Add timestamps for every documents save

--- backend/src/models/astrologer.ts
+++ backend/src/models/astrologer.ts
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-const astrologerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const astrologerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    languages: {
+      type: [String],
+      required: true,
+    },
+    specialties: {
+      type: [String],
+      required: true,
+    },
+    image: {
+      type: String,
+    },
   },
-  gender: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  languages: {
-    type: [String],
-    required: true,
-  },
-  specialities: {
-    type: [String],
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Astrologer", astrologerSchema);
